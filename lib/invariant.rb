0690aef2d831deb34ea9dbbf6ee954f5d037faea
Bob Long <robertjflong@gmail.com>
2015-12-03 11:12:42 +0000

    fixup

--- lib/invariant.rb
+++ lib/invariant.rb
@@ -1,1 +1,15 @@
 require 'invariant/version'
+
+module Invariant
+  INVARIANT_FAILED = Class.new(StandardError)
+
+  def run_with_invariants(x = nil)
+    result = self.send(:run, x)
+    yield(result) ? result : (fail INVARIANT_FAILED)
+  end
+
+  def run_with_invariants!(x = nil)
+    result = self.send(:run!, x)
+    yield(result) ? result : (fail INVARIANT_FAILED)
+  end
+end
